8ba627d312764fa72db1f9ad3a4bbafdf92fb462
Diego Freniche <diego@freniche.com>
2023-11-16 14:05:59 +0100

    Update import.sh

--- import.sh
+++ import.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cd data && tar xvfz library.tgz && mongorestore --uri mongodb://localhost:27017/library --drop library  && cd library && cd ../..
+cd data && tar xvfz library.tgz
 
-mongosh --username admin --password mongodb
+mongorestore --uri mongodb://localhost:27017/library --username admin --password mongodb  --drop --authenticationDatabase=admin library
+
+cd ..
+
+mongosh mongodb://localhost:27017/library --username admin --password mongodb --authenticationDatabase=admin library
