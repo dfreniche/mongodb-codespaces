5b500ef972cc72c7987ff122e576b166f8cb29ca
Diego Freniche <diego@freniche.com>
2023-11-16 12:27:08 +0000

    Fixed docker-compose and import script

--- import.sh
+++ import.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-cd data && tar xvfz library.tgz && mongorestore --uri mongodb://localhost:27017/library --drop library  && cd library && cd ../.. && mongosh 
+cd data && tar xvfz library.tgz && mongorestore --uri mongodb://localhost:27017/library --drop library  && cd library && cd ../..
+
+mongosh --username admin --password mongodb
