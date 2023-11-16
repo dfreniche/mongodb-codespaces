c998d4534cea2be2b03f60bb77eae2d7a947dcbd
Diego Freniche <diego@freniche.com>
2023-11-16 13:12:15 +0000

    Fixed import again

--- import.sh
+++ import.sh
@@ -6,4 +6,4 @@ mongorestore --uri mongodb://localhost:27017/library --username admin --password
 
 cd ..
 
-mongosh mongodb://localhost:27017/library --username admin --password mongodb --authenticationDatabase=admin library
+mongosh mongodb://localhost:27017/library --username admin --password mongodb --authenticationDatabase=admin
